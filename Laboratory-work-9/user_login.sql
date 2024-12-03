21dcf4f979729612cee6e54191ced3e32e19db3f
Alina <121886295+cupchik1@users.noreply.github.com>
2024-12-03 14:43:03 +0200

    docs: create user_login.sql

---
+++ Laboratory-work-9/user_login.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION user_login(client_ip INET, client_port INTEGER, process_id INTEGER)
+RETURNS VARCHAR AS $$
+DECLARE
+    token TEXT;
+BEGIN
+    token := md5(random()::TEXT || clock_timestamp()::TEXT);
+    
+    INSERT INTO token (token, client_ip, client_port, process_id) VALUES (token, client_ip, client_port, process_id);
+    RETURN token;
+END;
+$$ LANGUAGE plpgsql;
