c71d74251a22ddf4de60cd166c35357563d808f0
Alina <121886295+cupchik1@users.noreply.github.com>
2024-12-03 14:55:25 +0200

    fix: update user_login.sql

--- Laboratory-work-9/user_login.sql
+++ Laboratory-work-9/user_login.sql
@@ -1,11 +1,10 @@
-CREATE OR REPLACE FUNCTION user_login(client_ip INET, client_port INTEGER, process_id INTEGER)
-RETURNS VARCHAR AS $$
+CREATE OR REPLACE FUNCTION user_login(client_ip TEXT, client_port INTEGER, process_id INTEGER)
+RETURNS TEXT AS $$
 DECLARE
     token TEXT;
 BEGIN
     token := md5(random()::TEXT || clock_timestamp()::TEXT);
-    
-    INSERT INTO token (token, client_ip, client_port, process_id) VALUES (token, client_ip, client_port, process_id);
+    INSERT INTO user_tokens(token, client_ip, client_port, process_id) VALUES (token, client_ip, client_port, process_id);
     RETURN token;
 END;
 $$ LANGUAGE plpgsql;
